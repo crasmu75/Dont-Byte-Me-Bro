ed01c3e635bcc706f29c6b2c0d008ee26692c536
crasmu75 <crasmu75@bruinmail.slcc.edu>
2015-04-08 15:31:37 -0600

    Client fixes

--- C# Files/ClientGui/ClientModel/ClientModel.cs
+++ C# Files/ClientGui/ClientModel/ClientModel.cs
@@ -12,10 +12,12 @@ namespace Model
     {
 		// The socket used to communicate with the server.  If no connection has been
 		// made yet, this is null.
-		private StringSocket socket;
+		private Socket socket;
 
 		// Register for this event to be motified when a line of text arrives.
 		public event Action<String> IncomingLineEvent;
+
+		private byte[] buffer;
 
 		/// <summary>
         /// Creates a not yet connected client model.
@@ -32,9 +34,11 @@ namespace Model
 		{
 			if (socket == null)
 			{
+				buffer = new byte[1024];
 				TcpClient client = new TcpClient(hostname, port);
-				socket = new StringSocket(client.Client, Encoding.ASCII);
-				socket.BeginReceive(LineReceived, null);
+				socket = client.Client;
+				socket.BeginReceive(buffer, 0, buffer.Length,
+									SocketFlags.None, LineReceived, buffer);
 			}
 		}
 
@@ -44,22 +48,40 @@ namespace Model
 		/// <param name="line"></param>
 		public void SendMessage(String line)
 		{
+			byte[] msg = new byte[1024];
+			msg = Encoding.ASCII.GetBytes(line);
 			if (socket != null)
 			{
-				socket.BeginSend(line + "\n", (e, p) => { }, null);
+				socket.BeginSend(msg, 0,msg.Length,SocketFlags.None, null, msg);
 			}
 		}
 
 		/// <summary>
 		/// Deal with an arriving line of text.
 		/// </summary>
-		private void LineReceived(String s, Exception e, object p)
+		private void LineReceived(IAsyncResult result)
 		{
-			if (IncomingLineEvent != null)
+			byte[] msg = (byte[])(result.AsyncState);
+			String s = Encoding.ASCII.GetString(msg);
+			int index;
+			// process separate messages according to placement of \n characters
+			while ((index = s.IndexOf('\n')) >= 0)
 			{
-				IncomingLineEvent(s);
+				// take the string from beginning to where \n occurs
+				String line = s.Substring(0, index);
+
+				if (IncomingLineEvent != null)
+				{
+					IncomingLineEvent(line);
+				}
+
+				// delete the completed message from what we received
+				s = s.Substring(index + 1);
 			}
-			socket.BeginReceive(LineReceived, null);
+
+			// Ask for some more data
+			socket.BeginReceive(buffer, 0, buffer.Length,
+				SocketFlags.None, LineReceived, buffer);
 		}
 	}
 }

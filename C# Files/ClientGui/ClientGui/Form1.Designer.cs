752b38bb5aeaf65772293c816d183c7eaab41fde
crasmu75 <camiller@lab5-80.users.coe.utah.edu>
2015-04-09 17:12:14 -0600

    fonts and stuff broz

--- C# Files/ClientGui/ClientGui/Form1.Designer.cs
+++ C# Files/ClientGui/ClientGui/Form1.Designer.cs
@@ -51,46 +51,52 @@
 			// label_welcome
 			// 
 			this.label_welcome.AutoSize = true;
-			this.label_welcome.Location = new System.Drawing.Point(116, 23);
+			this.label_welcome.Font = new System.Drawing.Font("Miramonte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_welcome.Location = new System.Drawing.Point(37, 12);
 			this.label_welcome.Name = "label_welcome";
-			this.label_welcome.Size = new System.Drawing.Size(203, 13);
+			this.label_welcome.Size = new System.Drawing.Size(350, 24);
 			this.label_welcome.TabIndex = 1;
 			this.label_welcome.Text = "Welcome to the Spreadsheet Application!";
 			// 
 			// button1
 			// 
+			this.button1.BackColor = System.Drawing.Color.GhostWhite;
+			this.button1.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.button1.Location = new System.Drawing.Point(176, 239);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "Connect";
-			this.button1.UseVisualStyleBackColor = true;
+			this.button1.UseVisualStyleBackColor = false;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// label_host
 			// 
 			this.label_host.AutoSize = true;
+			this.label_host.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label_host.Location = new System.Drawing.Point(97, 80);
 			this.label_host.Name = "label_host";
-			this.label_host.Size = new System.Drawing.Size(32, 13);
+			this.label_host.Size = new System.Drawing.Size(37, 16);
 			this.label_host.TabIndex = 6;
 			this.label_host.Text = "Host:";
 			// 
 			// label_welcome1
 			// 
 			this.label_welcome1.AutoSize = true;
-			this.label_welcome1.Location = new System.Drawing.Point(133, 36);
+			this.label_welcome1.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_welcome1.Location = new System.Drawing.Point(118, 36);
 			this.label_welcome1.Name = "label_welcome1";
-			this.label_welcome1.Size = new System.Drawing.Size(166, 13);
+			this.label_welcome1.Size = new System.Drawing.Size(193, 16);
 			this.label_welcome1.TabIndex = 7;
 			this.label_welcome1.Text = "Please fill out all fields to connect.";
 			// 
 			// label_username
 			// 
 			this.label_username.AutoSize = true;
+			this.label_username.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label_username.Location = new System.Drawing.Point(97, 111);
 			this.label_username.Name = "label_username";
-			this.label_username.Size = new System.Drawing.Size(58, 13);
+			this.label_username.Size = new System.Drawing.Size(69, 16);
 			this.label_username.TabIndex = 8;
 			this.label_username.Text = "Username:";
 			// 
@@ -104,9 +110,10 @@
 			// label_spreadsheet
 			// 
 			this.label_spreadsheet.AutoSize = true;
+			this.label_spreadsheet.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label_spreadsheet.Location = new System.Drawing.Point(97, 195);
 			this.label_spreadsheet.Name = "label_spreadsheet";
-			this.label_spreadsheet.Size = new System.Drawing.Size(101, 13);
+			this.label_spreadsheet.Size = new System.Drawing.Size(117, 16);
 			this.label_spreadsheet.TabIndex = 10;
 			this.label_spreadsheet.Text = "Spreadsheet Name:";
 			// 
@@ -120,18 +127,20 @@
 			// label_note
 			// 
 			this.label_note.AutoSize = true;
-			this.label_note.Location = new System.Drawing.Point(55, 149);
+			this.label_note.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_note.Location = new System.Drawing.Point(27, 146);
 			this.label_note.Name = "label_note";
-			this.label_note.Size = new System.Drawing.Size(324, 13);
+			this.label_note.Size = new System.Drawing.Size(373, 16);
 			this.label_note.TabIndex = 12;
 			this.label_note.Text = "Enter the name of the spreadsheet on the server you wish to open. ";
 			// 
 			// label_note1
 			// 
 			this.label_note1.AutoSize = true;
-			this.label_note1.Location = new System.Drawing.Point(38, 162);
+			this.label_note1.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_note1.Location = new System.Drawing.Point(2, 162);
 			this.label_note1.Name = "label_note1";
-			this.label_note1.Size = new System.Drawing.Size(362, 13);
+			this.label_note1.Size = new System.Drawing.Size(417, 16);
 			this.label_note1.TabIndex = 13;
 			this.label_note1.Text = "If you wish to open a new spreadsheet, enter the name you want it to have.";
 			// 
@@ -139,6 +148,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MintCream;
 			this.ClientSize = new System.Drawing.Size(422, 287);
 			this.Controls.Add(this.label_note1);
 			this.Controls.Add(this.label_note);
